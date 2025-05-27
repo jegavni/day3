12ce2a50ecf9780f6129a2a35de78cad88df2c3c
jegavni <jegatheeshchandra@gmail.com>
2025-05-27 10:10:37 +0530

    this is push

---
+++ bodmas.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int a,b,c,d,e,f,g;
+ cout<<"enter first number"<< endl;
+ cin>>a;
+ cout<<"enter second number"<< endl;
+ cin>>b;
+   c=a+b;  
+   cout<<"addition is"<< c<< endl;
+   d=a-b;
+    cout <<"substraction is "<<d<<endl;
+    e=a*b;
+    cout <<"multiplication is "<<e <<endl;
+    f=a/b;
+    cout<<"division is "<<f<<endl;
+    g=a%b;
+    cout<<"modlus is "<<g<<endl;
+
+
+    
+    
+
+}
---
+++ name.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+string s;
+int c;
+
+cout<<"enter your name"<<endl;
+cin>>s;
+cout<<"enter your age"<<endl;
+cin>>c;
+cout<<"your name is"<<s<<endl;
+cout<<"age:"<<c<<endl;
+
+return 0;
+
+}
